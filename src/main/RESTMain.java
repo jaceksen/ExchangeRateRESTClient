08558d7680457e0fd3a3e10411a76abdbb58dd82
Jacek Seń <jaceksen@gmail.com>
2017-04-14 14:04:00 +0200

    pierwszy commit

---
+++ src/main/RESTMain.java
@@ -0,0 +1,25 @@
+package main;
+
+/**
+ * Calls the RESTful web service at REST_URL defined below.
+ * @author jsen
+ *
+ */
+public class RESTMain {
+	
+	/**
+	 * Call the web service and display the response.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		final String REST_URL = "www.url.com";
+		
+		
+		//call the service and get the response object
+		
+		//process the response object
+		
+	}
+
+}
