d463315ace8562c2901fade8617912fe0505331f
Jacek Seń <jaceksen@gmail.com>
2017-04-21 17:42:34 +0200

    commit 1

--- src/main/RESTMain.java
+++ src/main/RESTMain.java
@@ -1,5 +1,10 @@
 package main;
 
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.StatusType;
+
 /**
  * Calls the RESTful web service at REST_URL defined below.
  * @author jsen
@@ -7,18 +12,26 @@ package main;
  */
 public class RESTMain {
 	
-	/**
-	 * Call the web service and display the response.
-	 * @param args
-	 */
+	public static final String REST_URL = "https://currency-api.appspot.com/api/EUR/PLN.json";
+	public static final int OK_STATUS = Response.Status.OK.getStatusCode();
+	
 	public static void main(String[] args) {
 		
-		final String REST_URL = "www.url.com";
-		
-		
 		//call the service and get the response object
+		Response response = ClientBuilder.newClient().target(REST_URL)
+				.request(MediaType.APPLICATION_JSON).get();
+		
 		
 		//process the response object
+		StatusType status = response.getStatusInfo();
+		int statusCode = status.getStatusCode();
+			if(statusCode == OK_STATUS) {
+				System.out.println(response.readEntity(String.class));
+			}
+			else {
+				System.out.printf("Service returned status \"%d %s\"\n", statusCode, status.getReasonPhrase());
+			}
+		
 		
 	}
 
