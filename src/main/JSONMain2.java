a3b0ca5f4c280c1b1b1e8d615ed606a624f1dad8
Jacek Seń <jaceksen@gmail.com>
2017-04-26 21:16:36 +0200

    OK - kursy EUR i USD

--- src/main/JSONMain2.java
+++ src/main/JSONMain2.java
@@ -1,17 +1,29 @@
 package main;
 
+import java.io.FileReader;
+
+import javax.json.Json;
 import javax.json.JsonException;
+import javax.json.JsonObject;
+import javax.json.stream.JsonParser;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class JSONMain2 {
 
-	public static final String JSON_FILE_NAME = "plik.json";
-	
 	public static void main(String[] args) throws JsonException{
 		
 		RESTMain2 restMain2 = new RESTMain2();
-		System.out.println("WYNIK2" + restMain2.dajJsona());
+		//System.out.println("WYNIK2" + restMain2.dajJsona());
 		
+		JSONObject obj = new JSONObject(restMain2.dajJsona());
+		Double kurs = obj.getDouble("amount");
+		System.out.println("Kurs EUR/PLN: " + kurs);
 		
+		JSONObject obj2 = new JSONObject(restMain2.dajJsona2());
+		Double kurs2 = obj2.getDouble("amount");
+		System.out.println("Kurs USD/PLN: " + kurs2);
 		
 		
 	}
--- src/main/RESTMain2.java
+++ src/main/RESTMain2.java
@@ -14,20 +14,11 @@ import javax.ws.rs.core.Response.StatusType;
 public class RESTMain2 {
 
 	public static final String REST_URL = "https://currency-api.appspot.com/api/EUR/PLN.json";
+	public static final String REST_URL2 = "https://currency-api.appspot.com/api/USD/PLN.json";
 	public static final int OK_STATUS = Response.Status.OK.getStatusCode();
 
-	private String wynik;
-
-	public String getWynik() {
-		return wynik;
-	}
-
-	public void setWynik(String wynik) {
-		this.wynik = wynik;
-	}
-
 	public String dajJsona() {
-		
+
 		// call the service and get the response object
 		Response response = ClientBuilder.newClient().target(REST_URL).request(MediaType.APPLICATION_JSON).get();
 
@@ -38,33 +29,29 @@ public class RESTMain2 {
 			// System.out.println(response.readEntity(String.class));
 			String wynik1;
 			wynik1 = response.readEntity(String.class);
-			//RESTMain2 restMain2 = new RESTMain2();
-			//restMain2.setWynik(response.readEntity(String.class));
-			//System.out.println("WYNIK" + restMain2.getWynik());
 			return wynik1;
 		} else {
 			return null;
 		}
-	}
-
-	public static void main(String[] args) {
+	} //koniec dajJsona
+	
+	
+	public String dajJsona2() {
 
 		// call the service and get the response object
-		Response response = ClientBuilder.newClient().target(REST_URL).request(MediaType.APPLICATION_JSON).get();
+		Response response = ClientBuilder.newClient().target(REST_URL2).request(MediaType.APPLICATION_JSON).get();
 
 		// process the response object
 		StatusType status = response.getStatusInfo();
 		int statusCode = status.getStatusCode();
 		if (statusCode == OK_STATUS) {
 			// System.out.println(response.readEntity(String.class));
-			RESTMain2 restMain2 = new RESTMain2();
-			restMain2.setWynik(response.readEntity(String.class));
-			System.out.println("WYNIK" + restMain2.getWynik());
-
+			String wynik1;
+			wynik1 = response.readEntity(String.class);
+			return wynik1;
 		} else {
-			System.out.printf("Service returned status \"%d %s\"\n", statusCode, status.getReasonPhrase());
+			return null;
 		}
-
-	}
+	} //koniec dajJsona2
 
 }
